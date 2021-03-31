77cd5ca2e98894f9b1f1e5f93d940c3861403f4d
H-H Moon <moonssang031@naver.com>
2021-03-31 13:38:02 +0900

    initial commit

---
+++ app/src/main/java/com/smuexample/anoymousclass/MainActivity.java
@@ -0,0 +1,31 @@
+package com.smuexample.anoymousclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button mButton1;
+    TextView mTextview1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton1 = findViewById(R.id.button1);
+        mTextview1 = findViewById(R.id.textView1);
+
+        mButton1.setOnClickListener(new MyOnClickListener());
+    }
+
+    private class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            mTextview1.setText("You clicked button1");
+        }
+    }
+}
